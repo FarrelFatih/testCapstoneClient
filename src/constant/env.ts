387747d2dd1e6d7914fe052a913bdb9e9b389977
Farrel <farrel22fatih@gmail.com>
2023-12-26 19:05:29 +0700

    first commit

---
+++ src/constant/env.ts
@@ -0,0 +1,5 @@
+export const nextUrl =
+  process.env.NEXT_PUBLIC_CLIENT || "http://localhost:3000";
+
+export const nextAPIUrl =
+  process.env.NEXT_PUBLIC_API || "http://localhost:8080/api";
---
+++ src/lib/slices/user-boat-slices.tsx
@@ -0,0 +1,56 @@
+import { StateCreator } from "zustand";
+import axios from "axios";
+import { nextAPIUrl } from "@/constant/env";
+
+export interface userBoatType {
+  id: string;
+  namaKapal: string;
+  jenisKapal: string;
+  panjangKapal: number;
+  kapalAktif: boolean;
+  dermaga: string;
+  createdAt: string;
+  users_id: string;
+  chassis: string;
+  kapasitasKapal: number;
+  noMesin: string;
+  tanggalBuat: string;
+  warna: string;
+}
+
+export interface UserBoatState {
+  boats: userBoatType[];
+  userBoats: userBoatType | null;
+  getUserBoat: (id: string) => Promise<void>;
+  getUserBoatById: (id: string) => Promise<void>;
+}
+
+export const userBoatSlice: StateCreator<UserBoatState> = (set, get) => ({
+  boats: [],
+  userBoats: null,
+  getUserBoat: async (id: string) => {
+    axios.defaults.withCredentials = true;
+    try {
+      const res = await axios.get(
+        `${nextAPIUrl}/kapal/user/getKapalById/${id}`,
+        {
+          withCredentials: true,
+        }
+      );
+      set({ boats: res.data });
+    } catch (err: any) {
+      console.log(err);
+    }
+  },
+  getUserBoatById: async (id: string) => {
+    try {
+      const res = await axios.get(
+        `${nextAPIUrl}/billing/company/getboatbyid/${id}`,
+        { withCredentials: true }
+      );
+      set({ userBoats: res.data });
+    } catch (err) {
+      console.log(err);
+    }
+  },
+});
